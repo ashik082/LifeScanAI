1010b9c2608410422d4d81ae5476ae4442f2df09
a3ash <mail.a3ash@gmail.com>
2026-01-06 01:32:02 +0600

    Revert "trying to deploy"
    This reverts commit ed0ac5fa8176d62692b31b61ba307fa43c9aa2a6.

--- backend/app.py
+++ backend/app.py
@@ -56,9 +56,4 @@ def predict():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == "__main__":
-    # app.run(debug=True) #for local
-    #for hosting start
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-    #for hosting end
+    app.run(debug=True)
